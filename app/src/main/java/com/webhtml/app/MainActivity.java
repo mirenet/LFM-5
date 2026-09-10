3174f2f706696d401c4117850168f7f67903c44f
mirenet <mireinternet@tutamail.com>
2026-09-11 01:32:57 +0200

    Add onPageFinished method to WebView client

--- app/src/main/java/com/webhtml/app/MainActivity.java
+++ app/src/main/java/com/webhtml/app/MainActivity.java
@@ -100,6 +100,12 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
+            public void onPageFinished(WebView view, String url) { 
+            super.onPageFinished(view, url);
+            webView.evaluateJavascript("window.webhtml = true;", null);
+            }
+
+            @Override
             public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
                 String urlStr = request.getUrl().toString();
                 if (!urlStr.contains("#")) {
